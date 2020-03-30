7f6571b87455b24e01edf85f069af3c3beb54f78
htmlkor <62685344+htmlkor@users.noreply.github.com>
2020-03-30 13:24:47 +0900

    Create window_cc.js

---
+++ window_cc.js
@@ -0,0 +1,18 @@
+var window_cc = {
+  DOMContentLoaded: function() {
+    // ...
+  }, 
+  load: function() {
+    // ...
+  }
+};
+
+window.addEventListener(
+    'DOMContentLoaded'
+    , window_cc.DOMContentLoaded
+); 
+
+window.addEventListener(
+    'load'
+    , window_cc.load
+);
