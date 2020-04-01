4cc664fabfefba3f5549e323b486b4588ea0a51c
htmlkor <62685344+htmlkor@users.noreply.github.com>
2020-04-01 09:43:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+/* 
 app.use(express.static(__dirname));
 
-// app.get('/foo/:id?', function(req, res) {});
-// app.get('/:id', function(req, res) {});
+app.get('/foo/:id?', function(req, res) {});
+app.get('/:id', function(req, res) {});
 
 app.listen(port, function() {
     console.log('Start!');
 });
+*/
