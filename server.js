3abb88b507de0b8f841238f463d2301559c7082c
htmlkor <62685344+htmlkor@users.noreply.github.com>
2020-04-01 09:42:42 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var uaParser = require('ua-parser-js');
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static(__dirname));
+
+// app.get('/foo/:id?', function(req, res) {});
+// app.get('/:id', function(req, res) {});
+
+app.listen(port, function() {
+    console.log('Start!');
+});
