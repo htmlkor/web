e3a135a48c88bb34965da638d674f0a90182db2c
htmlkor <62685344+htmlkor@users.noreply.github.com>
2020-04-02 10:29:38 +0900

    Create modifyRenderData.js

---
+++ modifyRenderData.js
@@ -0,0 +1,10 @@
+var modifyRenderData = {
+  data: {}, 
+  set: function(data) {
+    // this.setList();
+  }, 
+  setList: function() {},
+  getData: function(key) {
+    return this.data[key];
+  }
+};
